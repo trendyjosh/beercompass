a28ca7cebd061f2f21e9704e464e22f1fe34bdfd
trendyjosh <trendyjosh@gmail.com>
2026-08-26 11:02:41 +0100

    feat: make importer package visibility

--- internal/importer/importer.go
+++ internal/importer/importer.go
@@ -3,11 +3,11 @@ package importer
 const defaultBaseURL = "https://download.geofabrik.de/europe/great-britain-latest.osm.pbf"
 
 // Download OSM data from geofrabik (updated daily).
-func downloadOsm() {
+func DownloadOsm() {
 	//
 }
 
 // Parse the OSM data to extract only pub nodes.
-func parseOsm() {
+func ParseOsm() {
 	//
 }
