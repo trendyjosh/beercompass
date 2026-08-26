a19984a88df2c9dc9fbff60af9b548d868c1e22f
trendyjosh <trendyjosh@gmail.com>
2026-08-26 11:30:51 +0100

    fix: migrate unable to read migration files

--- go.mod
+++ go.mod
@@ -10,11 +10,15 @@ require (
 )
 
 require (
+	github.com/DataDog/czlib v0.0.0-20240814115052-86a9592b3985 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
+	github.com/lib/pq v1.10.9 // indirect
 	github.com/paulmach/orb v0.12.0 // indirect
+	github.com/paulmach/protoscan v0.2.1 // indirect
 	go.mongodb.org/mongo-driver v1.17.4 // indirect
 	golang.org/x/sync v0.18.0 // indirect
 	golang.org/x/text v0.31.0 // indirect
+	google.golang.org/protobuf v1.36.10 // indirect
 )
--- internal/db/migrate.go
+++ internal/db/migrate.go
@@ -1,21 +1,33 @@
 package db
 
 import (
+	"embed"
+	"errors"
 	"fmt"
 
 	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	"github.com/golang-migrate/migrate/v4/source/iofs"
 )
+
+//go:embed migrations/*.sql
+var migrationsFS embed.FS
 
 // Run database migrations
 func RunMigrations(databaseURL string) error {
-	m, err := migrate.New("file://migrations", databaseURL)
+	source, err := iofs.New(migrationsFS, "migrations")
 	if err != nil {
-		return fmt.Errorf("RunMigrations: creating migrator: %w", err)
+		return fmt.Errorf("db: loading embedded migrations: %w", err)
+	}
+
+	m, err := migrate.NewWithSourceInstance("iofs", source, databaseURL)
+	if err != nil {
+		return fmt.Errorf("db: creating migrator: %w", err)
 	}
 	defer m.Close()
 
-	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
-		return fmt.Errorf("RunMigrations: running migrations: %w", err)
+	if err := m.Up(); err != nil && !errors.Is(err, migrate.ErrNoChange) {
+		return fmt.Errorf("db: running migrations: %w", err)
 	}
 
 	return nil
